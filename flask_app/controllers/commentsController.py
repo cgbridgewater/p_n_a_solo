5bebb3c0f3636b1c8f8104b92514c0e0159ccaed
Chris Bridgewater <cgbridgewater@outlook.com>
2023-04-18 13:05:36 -0700

    first commit

---
+++ flask_app/controllers/commentsController.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+from flask_app import app
+from flask import redirect, session, request
+from flask_app.models.comments import Comment
+# from flask_app.models.activities import Activity
+
+
+### NEW ACTIVITY POST ACTION 
+@app.route('/getoutside/activities/<int:id>/comment', methods=["POST"])
+def comment_form_action(id):
+    if 'user_id' not in session:
+        msg = "you must be logged in!"
+        return redirect('/logout')
+    data = {
+        "activity_id" : id,
+        "text" : request.form["text"],
+        "commenter" : request.form["commenter"],
+        }
+    if not Comment.comment_validation_check(data):
+        return redirect(f'/getoutside/activity/{id}') 
+    Comment.create_comment(data)
+    pprint(data)
+    return redirect(f'/getoutside/activity/{id}') 
---
+++ flask_app/models/comments.py
@@ -0,0 +1,55 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import datetime
+from pprint import pprint
+
+### COMMMENT CLASS
+class Comment:
+    def __init__(self,data):
+        self.id = data['id']
+        self.text = data['text']
+        self.commenter = data['commenter']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+### VALIDATE COMMENT
+    @staticmethod
+    def comment_validation_check(comment):
+        is_valid = True
+        if len(comment['text']) < 3:
+            flash("Comment must be at least 3 charactors", "comment")
+            is_valid = False
+        return is_valid 
+
+
+### CREATE COMMENT FORM ACTION
+    @classmethod
+    def create_comment(cls,data):
+        query = """
+            INSERT INTO comments (activity_id, text, commenter)
+            VALUES (%(activity_id)s, %(text)s, %(commenter)s)
+        """
+        return connectToMySQL('test_app').query_db(query,data)
+
+
+### GET COMMENTS BY ACTIVITY ID
+    @classmethod
+    def get_comments_by_activity_id(cls,data):
+        query = """
+            SELECT * FROM comments WHERE activity_id = %(id)s;
+        """
+        result = connectToMySQL("test_app").query_db(query,data)
+        pprint(result)
+        all_comments = []
+        for row in result:
+            one_comment = cls({
+                "id": row['id'],
+                "activity_id": row['activity_id'],
+                "commenter" : row['commenter'],
+                "text" : row['text'],
+                "created_at" : row['created_at'],
+                "updated_at" : row['updated_at'],
+            })
+            all_comments.append(one_comment)
+        return all_comments
